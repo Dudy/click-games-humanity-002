b3c0a1b47c5202fe2f702cb5339be581d4f4fa66
Dirk Podolak <dirk.podolak@essquare.de>
2020-06-11 13:26:05 +0200

    Updates

---
+++ precache-manifest.e18ea2396bfa92b2ab7dfcb5b88e66bc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffae8cc27c1d4948460ae5e52531d1e4",
+    "url": "/click-games-humanity-002/index.html"
+  },
+  {
+    "revision": "a21c98255899863bfa46",
+    "url": "/click-games-humanity-002/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "46caced0ee1aebfcc01a",
+    "url": "/click-games-humanity-002/static/js/2.fdf8da83.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/click-games-humanity-002/static/js/2.fdf8da83.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a21c98255899863bfa46",
+    "url": "/click-games-humanity-002/static/js/main.a0fff5f0.chunk.js"
+  },
+  {
+    "revision": "5f90a54625a39f15aa97",
+    "url": "/click-games-humanity-002/static/js/runtime-main.497252ef.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/click-games-humanity-002/static/media/logo.5d5d9eef.svg"
+  }
+]);
